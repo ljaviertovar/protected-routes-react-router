80d00beaf87d8f6cf92c96b86051c13afeff2f79
ljaviertovar <luisjavier_tovar@outlook.com>
2023-06-07 11:01:48 -0400

    add protected routes

--- src/pages/ConfigPage.jsx
+++ src/pages/ConfigPage.jsx
@@ -1,7 +1,7 @@
 export default function UsersPage() {
 	return (
 		<div>
-			<h1>ConfigPage (Private & config-role)</h1>
+			<h1>ConfigPage (Private & admin-role)</h1>
 		</div>
 	)
 }
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,7 +1,7 @@
 export default function DashboardPage() {
 	return (
 		<div>
-			<h1>DashboardPage (Private)</h1>
+			<h1>DashboardPage (Private && dashboard-permission)</h1>
 		</div>
 	)
 }
